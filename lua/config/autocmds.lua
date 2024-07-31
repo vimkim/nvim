c5b60ffaef2dcd904451fd452d97160d89ce3ad1
vimkim <kimdhyungg@gmail.com>
2024-07-31 10:45:02 +0900

    set tab width to 8 when c/cpp for gnu standards

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+-- Set tab settings specifically for C and C++ files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.bo.tabstop = 8
+    vim.bo.shiftwidth = 8
+    vim.bo.softtabstop = 8
+    vim.bo.expandtab = false
+  end,
+})
