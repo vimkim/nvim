ac540a700cf93560c2f7a2fe043ac181038d0c8a
Daehyun Kim <kimdhyungg@gmail.com>
2024-10-06 22:35:55 +0900

    feat(conform): markdown lint2 + toc

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -16,12 +16,35 @@ return {
           fish = { "fish_indent" },
           sh = { "shfmt" },
           zsh = { "beautysh" },
+
+          -- markdown
+          markdown = { "prettier", "markdownlint-cli2", "markdown-toc" },
+          ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
         },
         -- The options you set here will be merged with the builtin formatters.
         -- You can also define any custom formatters here.
         ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
         formatters = {
           injected = { options = { ignore_errors = true } },
+
+          -- markdown
+          ["markdown-toc"] = {
+            condition = function(_, ctx)
+              for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+                if line:find("<!%-%- toc %-%->") then
+                  return true
+                end
+              end
+            end,
+          },
+          ["markdownlint-cli2"] = {
+            condition = function(_, ctx)
+              local diag = vim.tbl_filter(function(d)
+                return d.source == "markdownlint"
+              end, vim.diagnostic.get(ctx.buf))
+              return #diag > 0
+            end,
+          },
           -- # Example of using dprint only when a dprint.json file is present
           -- dprint = {
           --   condition = function(ctx)
