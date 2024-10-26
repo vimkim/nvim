da080db1c003bd8d80fb998ca05806b627b82d37
Daehyun Kim <kimdhyungg@gmail.com>
2024-10-26 20:39:54 +0900

    feat(telescope): live grep with args

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,13 +2,53 @@
 return {
   {
     "telescope.nvim",
-    opts = {
-      defaults = {
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        event = "VeryLazy",
+        config = function(_, _)
+          require("lazyvim.util").on_load("telescope.nvim", function()
+            require("telescope").load_extension("live_grep_args")
+          end)
+        end,
+      },
+    },
+    opts = function(_, opts)
+      local lga_actions = require("telescope-live-grep-args.actions")
+      opts.extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        },
+      }
+      opts.defaults = {
         layout_config = {
           height = 0.95,
           width = 0.98,
         },
+      }
+    end,
+    keys = {
+      {
+        "<leader>/",
+        "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+        desc = "Grep (root dir)",
       },
     },
+    config = function(_, opts)
+      local tele = require("telescope")
+      tele.setup(opts)
+      tele.load_extension("live_grep_args")
+    end,
   },
 }
