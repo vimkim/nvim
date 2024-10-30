d990bab84e482787c9f57312ef439575440e6aba
Daehyun Kim <kimdhyungg@gmail.com>
2024-10-30 12:31:37 +0900

    fix(conform): wrong opts for conform, due to rtfm

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,65 +1,60 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = function()
-      local plugin = require("lazy.core.config").plugins["conform.nvim"]
-      ---@type conform.setupOpts
-      local opts = {
-        default_format_opts = {
-          timeout_ms = 3000,
-          async = false, -- not recommended to change
-          quiet = false, -- not recommended to change
-          lsp_format = "fallback", -- not recommended to change
-        },
-        formatters_by_ft = {
-          lua = { "stylua" },
-          fish = { "fish_indent" },
-          sh = { "shfmt" },
-          zsh = { "beautysh" },
-          rst = { "rstfmt" },
+    opts = {
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false, -- not recommended to change
+        quiet = false, -- not recommended to change
+        lsp_format = "fallback", -- not recommended to change
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
+        zsh = { "beautysh" },
+        rst = { "rstfmt" },
 
-          -- markdown
-          markdown = { "prettier", "markdownlint-cli2", "markdown-toc" },
-          ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-        },
-        -- The options you set here will be merged with the builtin formatters.
-        -- You can also define any custom formatters here.
-        ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-        formatters = {
-          injected = { options = { ignore_errors = true } },
+        -- markdown
+        markdown = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+      },
+      -- The options you set here will be merged with the builtin formatters.
+      -- You can also define any custom formatters here.
+      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+      formatters = {
+        injected = { options = { ignore_errors = true } },
 
-          -- markdown
-          ["markdown-toc"] = {
-            condition = function(_, ctx)
-              for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
-                if line:find("<!%-%- toc %-%->") then
-                  return true
-                end
+        -- markdown
+        ["markdown-toc"] = {
+          condition = function(_, ctx)
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+              if line:find("<!%-%- toc %-%->") then
+                return true
               end
-            end,
-          },
-          ["markdownlint-cli2"] = {
-            condition = function(_, ctx)
-              local diag = vim.tbl_filter(function(d)
-                return d.source == "markdownlint"
-              end, vim.diagnostic.get(ctx.buf))
-              return #diag > 0
-            end,
-          },
-          -- # Example of using dprint only when a dprint.json file is present
-          -- dprint = {
-          --   condition = function(ctx)
-          --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-          --   end,
-          -- },
-          --
-          -- # Example of using shfmt with extra args
-          -- shfmt = {
-          --   prepend_args = { "-i", "2", "-ci" },
-          -- },
+            end
+          end,
         },
-      }
-      return opts
-    end,
+        ["markdownlint-cli2"] = {
+          condition = function(_, ctx)
+            local diag = vim.tbl_filter(function(d)
+              return d.source == "markdownlint"
+            end, vim.diagnostic.get(ctx.buf))
+            return #diag > 0
+          end,
+        },
+        -- # Example of using dprint only when a dprint.json file is present
+        -- dprint = {
+        --   condition = function(ctx)
+        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+        --   end,
+        -- },
+        --
+        -- # Example of using shfmt with extra args
+        -- shfmt = {
+        --   prepend_args = { "-i", "2", "-ci" },
+        -- },
+      },
+    },
   },
 }
