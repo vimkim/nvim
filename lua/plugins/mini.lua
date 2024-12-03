2e3afe86a010fcb2a25b721c098f5232bf6b28f5
Daehyun Kim <kimdhyungg@gmail.com>
2024-12-03 15:24:50 +0900

    feat(mini.trailspace): highlight trailing spaces

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "echasnovski/mini.trailspace",
+    version = false,
+    lazy = true, -- needed for snacks.nvim - 1
+    event = "BufReadPost", -- needed for snacks.nvim - 2
+    opts = {}, -- needed for startup, same as require("mini.trailspace").setup({})
+  },
+}
