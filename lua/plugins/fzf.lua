417064e25cbac648c623389fb686ebf2ef9a41d7
Daehyun Kim <kimdhyungg@gmail.com>
2024-12-22 13:37:10 +0900

    alias(fzf): change alt i and alt d

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,9 +1,23 @@
+local actions = require("fzf-lua.actions")
 return {
   {
     "ibhagwan/fzf-lua",
     opts = {
       winopts = {
         fullscreen = true,
+      },
+      files = {
+        cwd_prompt = false,
+        actions = {
+          ["alt-i"] = { actions.toggle_ignore },
+          ["alt-d"] = { actions.toggle_hidden },
+        },
+      },
+      grep = {
+        actions = {
+          ["alt-i"] = { actions.toggle_ignore },
+          ["alt-d"] = { actions.toggle_hidden },
+        },
       },
     },
   },
