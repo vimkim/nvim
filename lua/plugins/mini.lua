ba5bfe8af67b1825e5efd9f681a7a430c3adf81e
Daehyun Kim <kimdhyungg@gmail.com>
2024-12-22 13:36:55 +0900

    feat(mini): disable trailing red

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,9 +1,11 @@
-return {
-  {
-    "echasnovski/mini.trailspace",
-    version = false,
-    lazy = true, -- needed for snacks.nvim - 1
-    event = "BufReadPost", -- needed for snacks.nvim - 2
-    opts = {}, -- needed for startup, same as require("mini.trailspace").setup({})
-  },
-}
+return {}
+
+-- return {
+--   {
+--     "echasnovski/mini.trailspace",
+--     version = false,
+--     lazy = true, -- needed for snacks.nvim - 1
+--     event = "BufReadPost", -- needed for snacks.nvim - 2
+--     opts = {}, -- needed for startup, same as require("mini.trailspace").setup({})
+--   },
+-- }
