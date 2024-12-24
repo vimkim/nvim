4cecdea3e54715aa8b34633aee27f48fb440071b
Daehyun Kim <kimdhyungg@gmail.com>
2024-12-24 13:59:48 +0900

    fix(lspsaga): disable annoying lightbulbs

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -2,7 +2,11 @@ return {
   {
     "nvimdev/lspsaga.nvim",
     config = function()
-      require("lspsaga").setup({})
+      require("lspsaga").setup({
+        lightbulb = {
+          enable = false,
+        },
+      })
     end,
     dependencies = {
       "nvim-treesitter/nvim-treesitter", -- optional
