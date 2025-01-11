15fad15f9b7d630faf725ac12e28451aebd50b56
Daehyun Kim <kimdhyungg@gmail.com>
2025-01-12 01:36:21 +0900

    revert(autocmd): disable formatting on c/cpp crucial. Sometimes .nvim.lua does not work and it ruins

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,23 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "c", "cpp", "h", "hpp", "cmake" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+
+-- Set tab settings specifically for C and C++ files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp", "h", "hpp", "cc", "hh" },
+  callback = function()
+    vim.bo.cindent = true
+    vim.bo.indentexpr = ""
+    vim.bo.cinoptions = "j1,f0,^-2,{2,>4,:4,n-2,(0,t0"
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 8
+    vim.bo.expandtab = false
+  end,
+})
