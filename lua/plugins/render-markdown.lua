b58f65633b0506ebad96407aa8db58b25be85732
Daehyun Kim <kimdhyungg@gmail.com>
2025-06-06 14:34:54 +0900

    feat(render-markdown): disable code block rendering

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,18 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    code = {
+      enabled = false, -- I added this.
+      sign = false,
+      width = "block",
+      right_pad = 1,
+    },
+    heading = {
+      sign = false,
+      icons = {},
+    },
+    checkbox = {
+      enabled = false,
+    },
+  },
+}
