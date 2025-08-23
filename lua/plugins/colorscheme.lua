7b29ab0fb3993b2f83e033f569acf2769a78daca
Daehyun Kim <kimdhyungg@gmail.com>
2025-08-23 18:38:56 +0900

    fix(colorscheme): bufferline integration error in catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,53 +1,20 @@
 return {
   {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        grug_far = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-  },
-
-  -- Configure LazyVim to load gruvbox
-  {
     "LazyVim/LazyVim",
     opts = {
+      -- colorscheme = "catppuccin",
       colorscheme = "catppuccin-frappe",
     },
+  },
+  -- quick fix for get error caused by bufferline.nvim
+  -- https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3202799735
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
   },
 }
