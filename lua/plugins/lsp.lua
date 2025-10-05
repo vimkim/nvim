36e77e067ed8ea9ea2b1ba9095b650d927b19301
Daehyun Kim <kimdhyungg@gmail.com>
2025-10-06 00:47:25 +0900

    keymap(lsp): <space>K now shows lsp hover

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,7 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- disable a keymap
       keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { "<space>K", vim.lsp.buf.hover, desc = "LSP Hover" }
       opts.inlay_hints = { enabled = false }
       return opts
     end,
