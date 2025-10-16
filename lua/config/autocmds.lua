8f53fd521d946be6360b33c14e8f56c66405b671
Daehyun Kim <kimdhyungg@gmail.com>
2025-10-16 13:54:31 +0900

    fix(autocmd): do not disable b.autoformat on c/cpp (g.autoformat already set)

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,12 +2,12 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "c", "cpp", "h", "hpp", "cmake" },
-  callback = function()
-    vim.b.autoformat = false
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   pattern = { "c", "cpp", "h", "hpp", "cmake" },
+--   callback = function()
+--     vim.b.autoformat = false
+--   end,
+-- })
 
 -- Set tab settings specifically for C and C++ files
 -- vim.api.nvim_create_autocmd("FileType", {
