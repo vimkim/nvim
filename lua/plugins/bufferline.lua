399f12608feae7de9f35102d5e5455a987387a4d
Daehyun Kim <kimdhyungg@gmail.com>
2025-10-26 23:55:18 +0900

    style(bufferline): compact, without diagnostics

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,34 +3,30 @@ return {
   event = "VeryLazy",
   opts = {
     options = {
-      mode = "buffers",
       separator_style = "slant", -- "slant" | "thick" | "thin"
-      always_show_bufferline = false,
-      show_buffer_close_icons = true,
-      show_close_icon = true,
-      color_icons = true,
-      indicator = { style = "underline" }, -- "icon" | "underline"
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      -- color_icons = true,
+      indicator = { style = "icon" }, -- "icon" | "underline"
       hover = { enabled = true, delay = 100, reveal = { "close" } },
-      diagnostics = "nvim_lsp",
-      diagnostics_update_in_insert = false,
-      offsets = {
-        { filetype = "neo-tree", text = "File Explorer", highlight = "Directory", text_align = "left" },
-        { filetype = "snacks_layout_box" },
-      },
+      -- diagnostics = "nvim_lsp",
+      diagnostics = false,
+      -- diagnostics_update_in_insert = false,
       right_mouse_command = false, -- stop accidental closes
       middle_mouse_command = false,
       get_element_icon = function(opts)
         return LazyVim.config.icons.ft[opts.filetype]
       end,
-      max_name_length = 22,
-      max_prefix_length = 12,
+      padding = 0,
+      max_name_length = 10,
+      max_prefix_length = 6,
       truncate_names = true,
       -- optional numbers or pins:
       numbers = "ordinal",
     },
     highlights = {
       buffer_selected = { bold = true, italic = false },
-      diagnostic_selected = { bold = true },
+      -- diagnostic_selected = { bold = true },
       indicator_selected = { sp = "#89b4fa", underline = true }, -- tweak color if desired
     },
   },
