7473638dfaacfae7cf0b5348ce871bd603956609
Daehyun Kim <kimdhyungg@gmail.com>
2025-10-26 22:29:29 +0900

    update(lsp.lua): older keymap deprecated

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,27 @@
 return {
   -- LSP keymaps
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function(_, opts)
+  --     local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  --     -- disable a keymap
+  --     KEYS[#KEYS + 1] = { "k", FALSE }
+  --     keys[#keys + 1] = { "<space>K", vim.lsp.buf.hover, desc = "LSP Hover" }
+  --     opts.inlay_hints = { enabled = false }
+  --     return opts
+  --   end,
+  -- },
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- disable a keymap
-      keys[#keys + 1] = { "K", false }
-      keys[#keys + 1] = { "<space>K", vim.lsp.buf.hover, desc = "LSP Hover" }
-      opts.inlay_hints = { enabled = false }
-      return opts
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "<space>K", vim.lsp.buf.hover, desc = "LSP Hover" },
+            { "K", false },
+          },
+        },
+      },
+    },
   },
 }
