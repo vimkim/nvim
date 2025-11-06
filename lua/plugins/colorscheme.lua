df185c29407dcc7a4f0fba8d47a1a634d4cec82d
Daehyun Kim <kimdhyungg@gmail.com>
2025-11-07 02:01:24 +0900

    minor(colorscheme): comment out redundant bufferline

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,13 +9,13 @@ return {
   },
   -- quick fix for get error caused by bufferline.nvim
   -- https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3202799735
-  {
-    "akinsho/bufferline.nvim",
-    -- init = function()
-    --   local bufline = require("catppuccin.groups.integrations.bufferline")
-    --   function bufline.get()
-    --     return bufline.get_theme()
-    --   end
-    -- end,
-  },
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   init = function()
+  --     local bufline = require("catppuccin.groups.integrations.bufferline")
+  --     function bufline.get()
+  --       return bufline.get_theme()
+  --     end
+  --   end,
+  -- },
 }
