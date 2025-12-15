4387e864b84a903d7a206cc2cbb8516137b68958
Daehyun Kim <kimdhyungg@gmail.com>
2025-12-16 03:09:00 +0900

    minor(colorscheme): wave

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,8 +4,24 @@ return {
     opts = {
       -- colorscheme = "catppuccin",
       -- colorscheme = "catppuccin-frappe",
-      colorscheme = "catppuccin-macchiato",
+      -- colorscheme = "catppuccin-macchiato",
+      -- colorscheme = "rose-pine-moon",
+      colorscheme = "kanagawa-wave",
     },
+  },
+  { "EdenEast/nightfox.nvim" },
+  {
+    "projekt0n/github-nvim-theme",
+  },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
   },
   -- quick fix for get error caused by bufferline.nvim
   -- https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3202799735
